8360cf6c33e1058eaaf71ed08dd6b891054a1758
Vitor Albano <vitor.albano@cingo.local>
2022-07-21 09:13:32 -0300

    adicionando cursos de JavaScript ao github

--- type-number.js
+++ type-number.js
@@ -1,7 +1,24 @@
+// tipo Number
+
 const meuNumero = 3;
 
-const num1 = 10 - 5;
-const num2 = 2;
+const primeiroNumero = 1;
+const segundoNumero = 2;
 
-const total = num1 + num2;
-console.log(total)
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// NaN -> Not A Number (não é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
